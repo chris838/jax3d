b5a15b5ec06a70cd66698b4d71ac6ccb5f1df623
Chris <925254+chris838@users.noreply.github.com>
2023-01-29 23:03:19 +0000

    Missed out test skip

--- jax3d/projects/mobilenerf/stage2.py
+++ jax3d/projects/mobilenerf/stage2.py
@@ -1436,97 +1436,99 @@ for i in tqdm(range(step_init, training_iters_a + 1)):
 #%%
 gc.collect()
 
-render_poses = data['test']['c2w'][:len(data['test']['images'])]
-frames = []
-framemasks = []
-print("Testing")
-for p in tqdm(render_poses):
-  out = render_loop(camera_ray_batch(p, hwf), vars, test_batch_size)
-  frames.append(out[2])
-  framemasks.append(out[3])
-psnrs_test = [-10 * np.log10(np.mean(np.square(rgb - gt))) for (rgb, gt) in zip(frames, data['test']['images'])]
-print("Test set average PSNR: %f" % np.array(psnrs_test).mean())
+if run_test_set_evaluation:
 
-#%%
-import jax.numpy as jnp
-import jax.scipy as jsp
+	render_poses = data['test']['c2w'][:len(data['test']['images'])]
+	frames = []
+	framemasks = []
+	print("Testing")
+	for p in tqdm(render_poses):
+		out = render_loop(camera_ray_batch(p, hwf), vars, test_batch_size)
+		frames.append(out[2])
+		framemasks.append(out[3])
+	psnrs_test = [-10 * np.log10(np.mean(np.square(rgb - gt))) for (rgb, gt) in zip(frames, data['test']['images'])]
+	print("Test set average PSNR: %f" % np.array(psnrs_test).mean())
 
-def compute_ssim(img0,
-                 img1,
-                 max_val,
-                 filter_size=11,
-                 filter_sigma=1.5,
-                 k1=0.01,
-                 k2=0.03,
-                 return_map=False):
-  """Computes SSIM from two images.
-  This function was modeled after tf.image.ssim, and should produce comparable
-  output.
-  Args:
-    img0: array. An image of size [..., width, height, num_channels].
-    img1: array. An image of size [..., width, height, num_channels].
-    max_val: float > 0. The maximum magnitude that `img0` or `img1` can have.
-    filter_size: int >= 1. Window size.
-    filter_sigma: float > 0. The bandwidth of the Gaussian used for filtering.
-    k1: float > 0. One of the SSIM dampening parameters.
-    k2: float > 0. One of the SSIM dampening parameters.
-    return_map: Bool. If True, will cause the per-pixel SSIM "map" to returned
-  Returns:
-    Each image's mean SSIM, or a tensor of individual values if `return_map`.
-  """
-  # Construct a 1D Gaussian blur filter.
-  hw = filter_size // 2
-  shift = (2 * hw - filter_size + 1) / 2
-  f_i = ((jnp.arange(filter_size) - hw + shift) / filter_sigma)**2
-  filt = jnp.exp(-0.5 * f_i)
-  filt /= jnp.sum(filt)
+	#%%
+	import jax.numpy as jnp
+	import jax.scipy as jsp
 
-  # Blur in x and y (faster than the 2D convolution).
-  filt_fn1 = lambda z: jsp.signal.convolve2d(z, filt[:, None], mode="valid")
-  filt_fn2 = lambda z: jsp.signal.convolve2d(z, filt[None, :], mode="valid")
+	def compute_ssim(img0,
+									img1,
+									max_val,
+									filter_size=11,
+									filter_sigma=1.5,
+									k1=0.01,
+									k2=0.03,
+									return_map=False):
+		"""Computes SSIM from two images.
+		This function was modeled after tf.image.ssim, and should produce comparable
+		output.
+		Args:
+			img0: array. An image of size [..., width, height, num_channels].
+			img1: array. An image of size [..., width, height, num_channels].
+			max_val: float > 0. The maximum magnitude that `img0` or `img1` can have.
+			filter_size: int >= 1. Window size.
+			filter_sigma: float > 0. The bandwidth of the Gaussian used for filtering.
+			k1: float > 0. One of the SSIM dampening parameters.
+			k2: float > 0. One of the SSIM dampening parameters.
+			return_map: Bool. If True, will cause the per-pixel SSIM "map" to returned
+		Returns:
+			Each image's mean SSIM, or a tensor of individual values if `return_map`.
+		"""
+		# Construct a 1D Gaussian blur filter.
+		hw = filter_size // 2
+		shift = (2 * hw - filter_size + 1) / 2
+		f_i = ((jnp.arange(filter_size) - hw + shift) / filter_sigma)**2
+		filt = jnp.exp(-0.5 * f_i)
+		filt /= jnp.sum(filt)
 
-  # Vmap the blurs to the tensor size, and then compose them.
-  num_dims = len(img0.shape)
-  map_axes = tuple(list(range(num_dims - 3)) + [num_dims - 1])
-  for d in map_axes:
-    filt_fn1 = jax.vmap(filt_fn1, in_axes=d, out_axes=d)
-    filt_fn2 = jax.vmap(filt_fn2, in_axes=d, out_axes=d)
-  filt_fn = lambda z: filt_fn1(filt_fn2(z))
+		# Blur in x and y (faster than the 2D convolution).
+		filt_fn1 = lambda z: jsp.signal.convolve2d(z, filt[:, None], mode="valid")
+		filt_fn2 = lambda z: jsp.signal.convolve2d(z, filt[None, :], mode="valid")
 
-  mu0 = filt_fn(img0)
-  mu1 = filt_fn(img1)
-  mu00 = mu0 * mu0
-  mu11 = mu1 * mu1
-  mu01 = mu0 * mu1
-  sigma00 = filt_fn(img0**2) - mu00
-  sigma11 = filt_fn(img1**2) - mu11
-  sigma01 = filt_fn(img0 * img1) - mu01
+		# Vmap the blurs to the tensor size, and then compose them.
+		num_dims = len(img0.shape)
+		map_axes = tuple(list(range(num_dims - 3)) + [num_dims - 1])
+		for d in map_axes:
+			filt_fn1 = jax.vmap(filt_fn1, in_axes=d, out_axes=d)
+			filt_fn2 = jax.vmap(filt_fn2, in_axes=d, out_axes=d)
+		filt_fn = lambda z: filt_fn1(filt_fn2(z))
 
-  # Clip the variances and covariances to valid values.
-  # Variance must be non-negative:
-  sigma00 = jnp.maximum(0., sigma00)
-  sigma11 = jnp.maximum(0., sigma11)
-  sigma01 = jnp.sign(sigma01) * jnp.minimum(
-      jnp.sqrt(sigma00 * sigma11), jnp.abs(sigma01))
+		mu0 = filt_fn(img0)
+		mu1 = filt_fn(img1)
+		mu00 = mu0 * mu0
+		mu11 = mu1 * mu1
+		mu01 = mu0 * mu1
+		sigma00 = filt_fn(img0**2) - mu00
+		sigma11 = filt_fn(img1**2) - mu11
+		sigma01 = filt_fn(img0 * img1) - mu01
 
-  c1 = (k1 * max_val)**2
-  c2 = (k2 * max_val)**2
-  numer = (2 * mu01 + c1) * (2 * sigma01 + c2)
-  denom = (mu00 + mu11 + c1) * (sigma00 + sigma11 + c2)
-  ssim_map = numer / denom
-  ssim = jnp.mean(ssim_map, list(range(num_dims - 3, num_dims)))
-  return ssim_map if return_map else ssim
+		# Clip the variances and covariances to valid values.
+		# Variance must be non-negative:
+		sigma00 = jnp.maximum(0., sigma00)
+		sigma11 = jnp.maximum(0., sigma11)
+		sigma01 = jnp.sign(sigma01) * jnp.minimum(
+				jnp.sqrt(sigma00 * sigma11), jnp.abs(sigma01))
 
-# Compiling to the CPU because it's faster and more accurate.
-ssim_fn = jax.jit(
-    functools.partial(compute_ssim, max_val=1.), backend="cpu")
+		c1 = (k1 * max_val)**2
+		c2 = (k2 * max_val)**2
+		numer = (2 * mu01 + c1) * (2 * sigma01 + c2)
+		denom = (mu00 + mu11 + c1) * (sigma00 + sigma11 + c2)
+		ssim_map = numer / denom
+		ssim = jnp.mean(ssim_map, list(range(num_dims - 3, num_dims)))
+		return ssim_map if return_map else ssim
 
-ssim_values = []
-for i in range(len(data['test']['images'])):
-  ssim = ssim_fn(frames[i], data['test']['images'][i])
-  ssim_values.append(float(ssim))
+	# Compiling to the CPU because it's faster and more accurate.
+	ssim_fn = jax.jit(
+			functools.partial(compute_ssim, max_val=1.), backend="cpu")
 
-print("Test set average SSIM: %f" % np.array(ssim_values).mean())
+	ssim_values = []
+	for i in range(len(data['test']['images'])):
+		ssim = ssim_fn(frames[i], data['test']['images'][i])
+		ssim_values.append(float(ssim))
+
+	print("Test set average SSIM: %f" % np.array(ssim_values).mean())
 #%%
 #%% --------------------------------------------------------------------------------
 # ## Save weights
